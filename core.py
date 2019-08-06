fbe01a9997aa30e58138831eac723f3dd453f940
Marchingegno <matteo.marchisciana@mail.polimi.it>
2019-08-06 16:27:49 +0200

    Add randomness. Add phrases.

--- core.py
+++ core.py
@@ -1,7 +1,11 @@
+import random
+
 from PIL import Image, ImageDraw, ImageFont
 
+import constants
 
-def writeinimage(imagePath, x, y, text, size=100):
+
+def writeinimage(imagePath, x, y, text, size=70):
     image = Image.open(imagePath)
     font = ImageFont.truetype("22815_LCOUR.ttf", size)
     draw = ImageDraw.Draw(image)
@@ -9,4 +13,15 @@ def writeinimage(imagePath, x, y, text, size=100):
     image.show()
 
 
-writeinimage('pic.jpg', 250, 250, "ciaooo", 300)
+randsize = random.randint(constants.MINSIZE, constants.MAXSIZE)
+randx = random.randint(constants.MINX, constants.MAXX)
+randy = random.randint(constants.MINY, constants.MAXY)
+while 1700 > randy > 1400:
+    # this avoids putting the text in the middle of the buttons
+    randy = random.randint(constants.MINY, constants.MAXY)
+
+lines = open('phrases.txt').read().splitlines()
+randtext = random.choice(lines)
+writeinimage('pic1.png', randx, randy, randtext, size=randsize)
+
+print(randx, randy)
